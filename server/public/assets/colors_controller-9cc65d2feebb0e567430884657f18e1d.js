a88c06a625252d304cbc87745ffd30ccec86863d
Luke Hamilton <luke@cloud.com>
2015-02-20 23:50:27 -0500

    Removed compiled assets from version control

--- server/public/assets/colors_controller-9cc65d2feebb0e567430884657f18e1d.js
+++
@@ -1,5 +0,0 @@
-angular.module('app').controller({
-  colorsController: function($rootScope, $scope, $location) {
-    return console.log('colors controller');
-  }
-});
--- server/public/assets/config_controller-de00bb6d5cbcb93aa75a873ef8baa8a0.js
+++
@@ -1,7 +0,0 @@
-angular.module('app').controller({
-  configController: function($scope, configService) {
-    return $scope.init = function(config) {
-      return configService.load_config(config);
-    };
-  }
-});
